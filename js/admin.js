28024b33fd164dad5638ec81252b2e5403073f4c
dber8nhardt <bernhardt.domi-tim17@it.dhbw-ravensburg.de>
2018-02-26 15:27:15 +0100

    Florian Commit

--- js/admin.js
+++ js/admin.js
@@ -4,10 +4,62 @@ function saveInput() {
 }
 
 function myFunction() {
-    var x = document.getElementById("myTopnav");
+    var x = document.getElementById("Topnav");
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
         x.className = "topnav";
+    }
+}
+
+function updateTable(table, value) {
+    var rows = document.getElementsByClassName("tr menuElement");
+
+    for (var i = 0; i < rows.length; i++) {
+
+        var row = rows[i].childNodes;
+
+        for (var n = 1; n < row.length; n++) {
+            var node = row[n].firstChild;
+            if (node != null) {
+                if (value == "") {
+                    node.parentElement.parentElement.style.display = '';
+                } else {
+                    switch (table) {
+                        case "orders":
+                            if (node.id == "Contact" || node.id == "Items") {
+                                if (!node.innerHTML.toUpperCase().includes(value.toUpperCase())) {
+                                    node.parentElement.parentElement.style.display = 'none';
+                                }
+                            }
+
+                            break;
+                        case "menu":
+                            if (node.id == "Name" || node.id == "Description" || node.id == "Types" || node.id == "Tags") {
+                                if (!node.innerHTML.toUpperCase().includes(value.toUpperCase())) {
+                                    node.parentElement.parentElement.style.display = 'none';
+                                }
+                            }
+
+                            break;
+                        case "customers":
+                            if (node.id == "Contact" || node.id == "EMail") {
+                                if (!node.innerHTML.toUpperCase().includes(value.toUpperCase())) {
+                                    node.parentElement.parentElement.style.display = 'none';
+                                }
+                            }
+                            break;
+                        case "extras":
+                            if (node.id == "Name") {
+                                if (!node.innerHTML.toUpperCase().includes(value.toUpperCase())) {
+                                    node.parentElement.parentElement.style.display = 'none';
+                                }
+                            }
+
+                            break;
+                    }
+                }
+            }
+        }
     }
 }
