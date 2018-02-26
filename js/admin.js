04889bcb7693aec80ee612f69de49f08a140882b
dber8nhardt <bernhardt.domi-tim17@it.dhbw-ravensburg.de>
2018-02-26 15:15:10 +0100

    Topnavbar adjustment Drag and Drop

--- js/admin.js
+++ js/admin.js
@@ -2,3 +2,12 @@ function saveInput() {
     var search = document.getElementById("input").value;
     alert(search);
 }
+
+function myFunction() {
+    var x = document.getElementById("Topnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
