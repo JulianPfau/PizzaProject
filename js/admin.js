85fa07fa6ebc595bef68b756e3dfa3a77ca85332
dber8nhardt <bernhardt.domi-tim17@it.dhbw-ravensburg.de>
2018-02-26 15:22:08 +0100

    Topnavbar adjustment

--- js/admin.js
+++ js/admin.js
@@ -2,3 +2,12 @@ function saveInput() {
     var search = document.getElementById("input").value;
     alert(search);
 }
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
