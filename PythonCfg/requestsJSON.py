049a00a9090f4765e4d6289f6b437b072a8b868e
Nico <nrumsch@t-online.de>
2018-03-02 19:43:32 +0100

    Comment: What to send the server to use appendOrder

--- PythonCfg/requestsJSON.py
+++ PythonCfg/requestsJSON.py
@@ -1,5 +1,39 @@
 import json
 
+'''
+JSON-String to add Order to orders.json:
+	{
+		"request" : "newOrder",
+		"file" : "orders",
+		"jsonData" : [
+			{
+				"id": "",
+				"items": [
+					{
+						"name": "",
+						"size": "",
+						"price": "",
+						"extras": [],
+						"count": ""
+					}
+				],
+				"total": "",
+				"customerid": "",
+				"contact": {
+					"name": "",
+					"postcode": "",
+					"city": "",
+					"street": "",
+					"nr": "",
+					"phone": ""
+				},
+				"done": "0"
+			}
+		]
+	}
+Caution don't use vowel mutation!
+can't be handelt by the webserver
+'''
 def appendOrder(json_dir, request):
 	try:
 		with open(json_dir + request["file"] + ".json", "r") as f:
