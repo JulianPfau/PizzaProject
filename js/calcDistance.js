c62554d10e6ed554b874f5c800dccb76b35cfa43
rost <rost@infolox.de>
2018-03-06 10:05:17 +0100

    calc

--- js/calcDistance.js
+++ js/calcDistance.js
@@ -13,34 +13,34 @@ function calcDistance(plz_user) {
 }
 
 function ajaxPLZ(pizza, user) {
-  var xhttp = new XMLHttpRequest();
-  
-  xhttp.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-      //document.getElementById("ans").innerHTML = this.responseText; //wird aufgerufen sobald die Rückmeldung
-																	//vom server kam -> hier ifabfrage und einfärbung
-																	//und blocken des weiter machen.
-        var distance = this.responseText;
-        if(distance <= 20000){
-          document.getElementById("postcode").style.backgroundColor = 'green';
-          }
-          if(distance > 20000){
-              document.getElementById("postcode").style.backgroundColor = 'red';
-              var noChance = "Ihr Ort liegt leider nicht im Lieferradius. Sorry!";
-              document.getElementById("ans").innerHTML = noChance;
+    var xhttp = new XMLHttpRequest();
 
-    }
-  }
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            //document.getElementById("ans").innerHTML = this.responseText; //wird aufgerufen sobald die Rückmeldung
+            //vom server kam -> hier ifabfrage und einfärbung
+            //und blocken des weiter machen.
+            var distance = this.responseText;
+            if (distance <= 20000) {
+                document.getElementById("postcode").style.backgroundColor = 'green';
+            }
+            if (distance > 20000) {
+                document.getElementById("postcode").style.backgroundColor = 'red';
+                var noChance = "Ihr Ort liegt leider nicht im Lieferradius. Sorry!";
+                document.getElementById("ans").innerHTML = noChance;
+            }
+        }
 
-  var data = {
-    "request"   : "ajaxGoogleAPI",
-    "plz_pizza" : pizza,
-    "plz_user"  : user
-  };
-  parse = JSON.stringify(data);
-  xhttp.open("POST", "https://localhost:8080", true);
-  xhttp.send(parse);
-  
-}}
+    };
+    var data = {
+        "request": "ajaxGoogleAPI",
+        "plz_pizza": pizza,
+        "plz_user": user
+    };
+    parse = JSON.stringify(data);
+    xhttp.open("POST", "https://localhost:8080", true);
+    xhttp.send(parse);
+
+}
 
 
