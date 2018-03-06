634b6a10245bddef831477829a7f500ab0892eab
rost <rost@infolox.de>
2018-03-06 13:30:25 +0100

    order

--- js/conf1.js
+++ js/conf1.js
@@ -1,5 +1,5 @@
 
-function getdata(){
+function getdata() {
 
 
     var bestellung = sessionStorage.getItem('bestellung');
@@ -9,12 +9,12 @@ function getdata(){
     document.getElementById('name').value = bestellung["contact"]["name"];
     document.getElementById('adress').value = bestellung["contact"]["postcode"] + " " + bestellung["contact"]["city"];
     document.getElementById('street').value = bestellung["contact"]["street"] + " " + bestellung["contact"]["nr"];
-    document.getElementById('phone').value = bestellung["contact"]["phone"]
+    document.getElementById('phone').value = bestellung["contact"]["phone"];
     document.getElementById('total').value = bestellung["total"] + "\u20AC";
+    document.getElementById('payment').value = bestellung["contact"]["zahlung"];
 
     var pizzen = bestellung["items"];
     for (i = 0; i < pizzen.length; i++) {
         document.getElementById('pizzaname').value += bestellung[i]["items"]["name"] + "(" + bestellung[i]["items"]["size"] + ")" + " mit " + bestellung[i]["items"]["extras"] + "	  " + "\n";
     }
-    ;
 }
