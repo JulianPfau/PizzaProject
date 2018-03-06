1b78e1e36999090cb99c0ef9ebadd5456e9610d4
rost <rost@infolox.de>
2018-03-06 08:18:10 +0100

    bla

--- js/calcDistance.js
+++ js/calcDistance.js
@@ -16,7 +16,7 @@ function ajaxPLZ(pizza, user) {
   var xhttp = new XMLHttpRequest();
   
   xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
+    if (this.readyState === 4 && this.status === 200) {
       //document.getElementById("ans").innerHTML = this.responseText; //wird aufgerufen sobald die Rückmeldung
 																	//vom server kam -> hier ifabfrage und einfärbung
 																	//und blocken des weiter machen.
@@ -26,18 +26,19 @@ function ajaxPLZ(pizza, user) {
           }
           if(distance > 20000){
               document.getElementById("postcode").style.backgroundColor = 'red';
-          }
+              var noChance = "Ihr Ort liegt leider nicht im Lieferradius. Sorry!";
+              document.getElementById("ans").innerHTML = noChance;
 
     }
-  };
+  }
 
   var data = {
     "request"   : "ajaxGoogleAPI",
     "plz_pizza" : pizza,
     "plz_user"  : user
-  }
+  };
   parse = JSON.stringify(data);
   xhttp.open("POST", "https://localhost:8080", true);
   xhttp.send(parse);
   
-}
+}}
