1f7cb52211684e2c113c925044c9be038fde9e3a
rost <rost@infolox.de>
2018-03-06 09:43:30 +0100

    huhu

--- js/calcDistance.js
+++ js/calcDistance.js
@@ -9,35 +9,38 @@ function getPLZ() {
 
 function calcDistance(plz_user) {
   var plz_pizza = "88045+Fallenbrunn";
-  ajaxPLZ(plz_pizza, plz_user);
+  var xml = ajaxPLZ(plz_pizza, plz_user);
 }
 
 function ajaxPLZ(pizza, user) {
   var xhttp = new XMLHttpRequest();
+  
+  xhttp.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      //document.getElementById("ans").innerHTML = this.responseText; //wird aufgerufen sobald die Rückmeldung
+																	//vom server kam -> hier ifabfrage und einfärbung
+																	//und blocken des weiter machen.
+        var distance = this.responseText;
+        if(distance <= 20000){
+          document.getElementById("postcode").style.backgroundColor = 'green';
+          }
+          if(distance > 20000){
+              document.getElementById("postcode").style.backgroundColor = 'red';
+              var noChance = "Ihr Ort liegt leider nicht im Lieferradius. Sorry!";
+              document.getElementById("ans").innerHTML = noChance;
 
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("ans").innerHTML = this.responseText;
-            var distance = this.responseText;
-            if(distance <= 20000){
-                document.getElementById("postcode").style.backgroundColor = 'green';
-            }
-            if(distance > 20000) {
-                document.getElementById("postcode").style.backgroundColor = 'red';
-                //var noChance = "Ihr Ort liegt leider nicht im Lieferradius. Sorry!";
-                //document.getElementById("ans").innerHTML = noChance;
-
-            }}
-    };
+    }
+  }
 
   var data = {
     "request"   : "ajaxGoogleAPI",
     "plz_pizza" : pizza,
     "plz_user"  : user
   };
-
-  var parse = JSON.stringify(data);
+  parse = JSON.stringify(data);
   xhttp.open("POST", "https://localhost:8080", true);
   xhttp.send(parse);
+  
+}}
 
-}
+
