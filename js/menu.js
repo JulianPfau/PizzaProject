d3bf4697ce78e09691f139a2d74e0d9a6ae37083
JulianPfau <pfau.julian-tim17@it.dhbw-ravensburg.de>
2018-03-16 19:52:52 +0100

    added comments

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,24 @@
 var extras;
 
+/**
+ *
+ * Saves the JSON data to a global variable to use it later in the script
+ *
+ * @param json-Data from extras.json
+ */
 function getExtras(json) {
     extras = json;
 }
-
+/**
+ * Is called as callback function to generate the menu for the order page.
+ * @param rawData json Data from loadJSONfromServer function
+ */
 function createTablefromJSON(rawData) {
     loadJSONfromServer("extras", getExtras);
 
-    var json = JSON.parse(rawData)["jsonData"];
+    var json = JSON.parse(rawData)["jsonData"];                                       // parse the JSON string to an JSON-Object
     var table = document.getElementsByClassName("table")[0];
-    var arrContent = ["picture", "name", "description", "sizes", "extras", "prices"];
+    var arrContent = ["picture", "name", "description", "sizes", "extras", "prices"]; // define all column headers, is also used to set the order of columns
 
     //remove all rows if exists
     while (table.firstChild) {
@@ -17,10 +26,10 @@ function createTablefromJSON(rawData) {
     }
     //create elements from JSON file
 
-    for (var i = 0; i < json.length; i++) {
+    for (var i = 0; i < json.length; i++) {                                            // loop through the JSON Object
         var container = document.createElement("div");
         container.setAttribute("class", "col-12 col-md");
-        container.setAttribute("name", i + "_" + json[i]["name"]);
+        container.setAttribute("name", i + "_" + json[i]["name"]);                     //create a HTML Object and set the name by index and name of the product
         container.style.display = "flex";
 
         var btnOrder = document.createElement("button");
@@ -29,9 +38,9 @@ function createTablefromJSON(rawData) {
         selAmount.setAttribute("class", "selectpicker");
         var amId = 'amount' + i;
         selAmount.setAttribute("id", amId);
-        selAmount.setAttribute("onChange", "changePrice('" + i + "')");
+        selAmount.setAttribute("onChange", "changePrice('" + i + "')");                  //set onChange event with index of the element
 
-        for (var k = 1; k < 10; k++) {
+        for (var k = 1; k < 10; k++) {                                                   // create a HTML-Dropdown with a max value of 10 - for the amount of pizzas a user can order at once
             var option = document.createElement("option");
             option.setAttribute("value", k);
             option.innerHTML = k;
@@ -49,12 +58,12 @@ function createTablefromJSON(rawData) {
         paragraph = class mb-1 id = id  p. innerHTML key
         */
         // create col for json data
-        arrContent.forEach(function (key) {
+        arrContent.forEach(function (key) {                                         // for each key set above go through the element and create an div element
             var value = json[i][key];
             var d = document.createElement("div");
             d.setAttribute("class", "col-12 col-md");
             // if it's a picture create img src
-            if (key == "picture") {
+            if (key == "picture") {                                                 // set attribute to display an image in html
                 var content = document.createElement("img");
                 content.setAttribute("src", "/img/menu/" + value);
                 content.setAttribute("id", key);
@@ -70,7 +79,7 @@ function createTablefromJSON(rawData) {
                 content.setAttribute("class", "mb-1");
                 content.setAttribute("id", key);
                 //content.style.flexDirection = "column";
-                if (key == "sizes" && value.length != 0) {
+                if (key == "sizes" && value.length != 0) {                          // create an dropdown to select one of the different sizes
                     var sel = document.createElement("select");
                     sel.setAttribute("class", "selectpicker");
                     sel.setAttribute("onchange", "changePrice(" + i + ")");
@@ -83,11 +92,11 @@ function createTablefromJSON(rawData) {
                         sel.appendChild(opt);
                     }
                     content.appendChild(sel);
-                } else if (key == "prices") {
+                } else if (key == "prices") {                                       // set the default price to the first price of the product
                     content.innerHTML = "Preis: " + value[0] + " €";
 
                     for (var p = 0; p < value.length; p++) {
-                        var input = document.createElement("input");
+                        var input = document.createElement("input");                // create hidden input fields for additional prices
                         input.setAttribute("type", "hidden");
                         input.setAttribute("name", "price" + p);
                         input.value = value[p];
@@ -99,7 +108,7 @@ function createTablefromJSON(rawData) {
 
                     for (var e = 0; e < value.length; e++) {
                         try {
-                            var input = document.createElement("input");
+                            var input = document.createElement("input");            // create hidden input fields for additional extras
                             input.setAttribute("type", "hidden");
                             input.setAttribute("name", e);
                             input.value = arrExtras[value[e] - 1].price;
@@ -125,46 +134,51 @@ function createTablefromJSON(rawData) {
 
         });
 
-        // append finished row to table
-        //container.appendChild(col);
+                                                                                    // append finished row to table
         container.appendChild(selAmount);
         container.appendChild(btnOrder);
         table.appendChild(container);
     }
 }
 
+/**
+ * Function to call the Extras popup
+ * @param element the span-HTML-Object of the product
+ * @param index   row index of the product
+ */
+
 function openExtras(element, index) {
     var extras = element.parentNode.getElementsByTagName("input");
-    //Sets title of Extras Popup
-    document.getElementById("modalExtrasItems").innerHTML = "Extras";
-    //Sets index for write
-    var extrasBox = document.getElementById("extrasBox");
+
+    document.getElementById("modalExtrasItems").innerHTML = "Extras";               //Sets title of Extras Popup
+
+    var extrasBox = document.getElementById("extrasBox");                           //Sets index for write
 
     extrasBox.setAttribute('name', index);
 
-    //Removes all Extras from Popup
-    while (extrasBox.firstChild) {
+
+    while (extrasBox.firstChild) {                                                  //Removes all Extras from Popup
         extrasBox.removeChild(extrasBox.firstChild);
     }
 
-    //Loops every extra from input fields
-    for (var i = 0; i < extras.length; i++) {
-        //Creates DOM-Elements for Popup
-        var li = document.createElement('li');
+
+    for (var i = 0; i < extras.length; i++) {                                       //Loops every extra from input fields
+
+        var li = document.createElement('li');                                      //Creates DOM-Elements for Popup
         var label = document.createElement('label');
         var input = document.createElement('input');
         var span = document.createElement('span');
 
-        //Defines the checkbox
-        input.setAttribute('type', 'checkbox');
+
+        input.setAttribute('type', 'checkbox');                                     //Defines the checkbox
         input.style.listStyleType = "none";
 
         var extraCheckboxId = index + ":" + i;
 
         input.setAttribute('id', extraCheckboxId);
 
-        //Content & Change Event for extra
-        span.innerHTML = " " + extras[i].innerText + " " + parseFloat(extras[i].value).toFixed(2) + "€";
+
+        span.innerHTML = " " + extras[i].innerText + " " + parseFloat(extras[i].value).toFixed(2) + "€";    //Content & Change Event for extra
         span.onkeydown = function () {
             var keys = [16, 17, 18, 20, 33, 34, 35, 36, 37, 38, 39, 40, 45, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 144, 145];
             if (!keys.includes(event.keyCode)) {
@@ -174,8 +188,8 @@ function openExtras(element, index) {
         };
 
 
-        //Checks weather the extras is already in selected or not and it'll be marked if so
-        if (extras[i].getAttribute("selected") == "true") {
+
+        if (extras[i].getAttribute("selected") == "true") {                         //Checks weather the extras is already in selected or not and it'll be marked if so
             input.setAttribute('checked', 'true');
         }
 
@@ -189,16 +203,21 @@ function openExtras(element, index) {
 
 }
 
-function confirmExtras(element) {
+/**
+ * Function to save the extras selection of the User by calling the modal HTML-Object and read the selectet data
+ *
+ */
+
+function confirmExtras() {
 
     var modal = document.getElementById("extrasBox");
     var extraInputs = document.getElementsByTagName("table")[0].childNodes[modal.getAttribute("name")].childNodes[4].getElementsByTagName("input");
 
     var checkboxes = modal.getElementsByTagName("input");
 
-    for (var i = 0; i < checkboxes.length; i++) {
+    for (var i = 0; i < checkboxes.length; i++) {                               //loop throw all checkboxes
 
-        if (checkboxes[i].checked) {
+        if (checkboxes[i].checked) {                                            // if the checkbox is selected set the input field of the extra in the product row to selected
 
             extraInputs[i].setAttribute("selected", "true");
 
@@ -211,9 +230,14 @@ function confirmExtras(element) {
     document.getElementById("closeModal").click();
 }
 
+/**
+ * This Function is called to update the price of the transferred product
+ * @param element index of the table row
+ */
 
 function changePrice(element) {
-    var table = document.getElementsByTagName("table")[0].childNodes;
+
+    var table = document.getElementsByTagName("table")[0].childNodes;                           // set all variables for later usage
     var size = table[element].childNodes[3].getElementsByTagName("select")[0].value;
     var extras = table[element].childNodes[4].getElementsByTagName("input");
     var priceCol = table[element].childNodes[5];
@@ -221,19 +245,19 @@ function changePrice(element) {
     var amount = 0;
     var pizzaPrice = table[element].childNodes[5].getElementsByTagName("input")[size].value;
 
-    for (var i = 0; i < extras.length; i++) {
+    for (var i = 0; i < extras.length; i++) {                                                   //loop through all extras
 
         if (extras[i].getAttribute("selected") == "true") {
-            extraPrice = parseFloat(extraPrice) + parseFloat(extras[i].value);
+            extraPrice = parseFloat(extraPrice) + parseFloat(extras[i].value);                  // if an extra is selected add the price to total
         }
     }
-    if (table[element].getElementsByTagName("select")[1].value != 0) {
+    if (table[element].getElementsByTagName("select")[1].value != 0) {                          // call the amount the user has selected
 
         amount = table[element].getElementsByTagName("select")[1].value;
     } else {
-        amount = 1;
+        amount = 1;                                                                             // if the amount is 0 set the value to 1 to display the single price of the product
     }
     var price = ((parseFloat(pizzaPrice) + parseFloat(extraPrice)) * amount).toFixed(2);
-    priceCol.getElementsByTagName("span")[0].innerHTML = "Preis: " + price + " €";
+    priceCol.getElementsByTagName("span")[0].innerHTML = "Preis: " + price + " €";              // set the inner of the HTML-span to the new price value
 
 }
