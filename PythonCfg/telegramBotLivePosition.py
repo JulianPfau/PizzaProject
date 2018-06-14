f3e22cc20f08990259fce7f5683e0facb0c9d9e1
Nico <nrumsch@t-online.de>
2018-06-14 19:48:52 +0200

    Changed Function of the live location feed should work now^^

--- PythonCfg/telegramBotLivePosition.py
+++ PythonCfg/telegramBotLivePosition.py
@@ -2,11 +2,35 @@ import json
 import os
 import ajaxGoogleAPI
 
-messages = {}  # {"Drivers_ID": {"message": messages, "sendTo": {}}}
+server_dir = os.path.dirname(os.path.abspath(__file__))
+server_root = os.path.sep.join(server_dir.split(os.path.sep)[:-1])
+img_dir = server_root + "/img/"
+json_dir = server_root + "/json/"
+pdf_dir = server_root + "/pdf/"
 
+active_deliverys = {}
+
+def get_json(file):
+    try:
+        with open(json_dir + file + ".json") as json_data:
+            d = json.load(json_data)
+        response = {
+            'STATUS': 'OK',
+            'jsonData': d
+        }
+        return response
+    except IOError:
+        return False
+
+def write_json(filename, data):
+    try:
+        with open(json_dir + filename + ".json", "w") as json_data:
+            json.dump(data, json_data)
+        return True
+    except IOError:
+        return False
 
 def get(bot, update):
-    get_drivers_for_all_order()
     location = None
     chat_id = None
     if update.edited_message:
@@ -15,80 +39,61 @@ def get(bot, update):
     elif update.message:
         chat_id = update.message.chat.id
         location = update.message.location
+    
+    orders = get_json("orders")["jsonData"]
+    for item in orders:
+        if item["contact"]["chat_id"] is not None and item["driver"] is None and not item["delivered"]:
+            drivers = get_json("driver")["jsonData"]
 
-    if is_driver(chat_id):
-        messages[chat_id]["message"] = update
-        for to_id in messages[chat_id]["sendTo"]:
-            if messages[chat_id]["sendTo"][to_id]:
-                bot.editMessageLiveLocation(chat_id=to_id,
-                                            message_id=messages[chat_id]["sendTo"][to_id]["message_id"],
-                                            latitude=location.latitude, longitude=location.longitude)
-            else:
-                messages[chat_id]["sendTo"][to_id] = bot.sendLocation(chat_id=to_id, latitude=location.latitude,
+            for driver in drivers:
+                if drivers[driver]["available"]:
+                    item["driver"] = driver
+                    drivers[driver]["available"] = False
+                    drivers[driver]["order_id"] = item["id"]
+                    active_deliverys[chat_id] = bot.sendLocation(chat_id=chat_id, latitude=location.latitude,
                                                                       longitude=location.longitude,
-                                                                      disable_notification=True, live_period=2700)
-
-
-def deliver(bot, update, args):
-    if is_driver(update.message.chat.id) and args:
-        order_number = args[0]
-
-        response = json.loads(get_json({"file": "orders"}))
-        if response["STATUS"] == "OK":
-            json_data = response["jsonData"]
-            for order in json_data:
-                if order["id"] == order_number:
-                    order["delivered"] = True
-                    if messages[update.message.chat.id]["sendTo"][order["contact"]["chat_id"]] is None:
-                        bot.stopMessageLiveLocation(chat_id=order["contact"]["chat_id"])
-                    else:
-                        messages[update.message.chat.id]["sendTo"].pop(order["contact"]["chat_id"])
-                        bot.stopMessageLiveLocation(chat_id=order["contact"]["chat_id"],
-                                                    message_id=messages[update.message.chat.id]["sendTo"][
-                                                        order["contact"]["chat_id"]])
+                                                                      disable_notification=True, live_period=2700)['message_id']
                     break
-
-
-def get_drivers_for_all_order():
-    response = json.loads(get_json({"file": "orders"}))
-    if response["STATUS"] == "OK":
-        orders = response["jsonData"]
-        for order in orders:
-            if not order["delivered"]:
-                if order["driver"]:
-                    if not order["driver"] in list(messages.keys()):
-                        messages[order["driver"]] = {"message": None, "sendTo": {}}
-
-                    if order["contact"]["chat_id"]:
-                        messages[order["driver"]]["sendTo"][order["contact"]["chat_id"]] = None
-    # print(messages)
-
-
+    
+    
+    
+            write_json("driver", drivers)
+    write_json("orders", orders)
+    
+    bot.editMessageLiveLocation(chat_id=chat_id, message_id = active_deliverys[chat_id] ,latitude=location.latitude, longitude=location.longitude)
+    
 def is_driver(chat_id):
-    response = json.loads(get_json({"file": "driver"}))
+    response = get_json("driver")
 
     if response["STATUS"] == "OK":
         json_data = response["jsonData"]
         return str(chat_id) in list(json_data.keys())
 
-
-def get_json(file):
-    try:
-        with open(os.path.sep.join(os.path.dirname(os.path.abspath(__file__)).split(os.path.sep)[:-1]) + "/json/" + file + ".json") as json_data:
-            d = json.load(json_data)
-        response = {
-            'STATUS': 'OK',
-            'jsonData': d
-        }
-        response = json.dumps(response)
-        return response
-    except IOError:
-        return False
-
+def deliver(bot, update, args):
+    if is_driver(update.message.chat.id) and args:
+        order_number = args[0]
+        response = get_json("orders")
+        if response["STATUS"] == "OK":
+            json_data = response["jsonData"]
+            for order in json_data:
+                if order["id"] == order_number and not order["delivered"]:
+                    order["delivered"] = True
+                    
+                    drivers = get_json("driver")["jsonData"]
+                    driver = drivers[order["driver"]]
+                    driver["available"] = True
+                    driver["order_id"] = None
+                    
+                    bot.send_message(chat_id=order["contact"]["chat_id"], text="Pizza wurde geliefert.")
+                    chat_id = order["contact"]["chat_id"]
+                    bot.stopMessageLiveLocation(chat_id=chat_id, message_id = active_deliverys[chat_id] )
+                    active_deliverys.pop(chat_id, None)
+                    write_json("driver", drivers) 
+        write_json("orders", json_data)
 
 def delivery_time(bot, update, args):
     chat_id = update.message.chat.id
-    orders = json.loads(get_json("orders"))
+    orders = get_json("orders")
     waypoints = []
     related_orders = []
     for order in orders['jsonData']: # loop trough all oders
