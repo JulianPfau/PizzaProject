a6bbd92986b02f406d61256e12e5cedc36b072bf
dber8 <d.ber8nhardt@hotmail.de>
2018-06-14 15:06:26 +0200

    - updated telegramBot.Start checking if user is already registered

--- PythonCfg/telegramBotStart.py
+++ PythonCfg/telegramBotStart.py
@@ -1,3 +1,4 @@
+# -*- coding: iso-8859-1 -*-
 import telegram
 import os
 import json
@@ -8,16 +9,27 @@ json_dir = server_root + "/json/"
 
 data = {}  # {chat_id:1, chat_id2: 3}
 users = {}
+password = []
 
 
 def start(bot, update):
-    global data, users
+    global data, users, registered
     chat_id = update.message.chat_id
 
-    if chat_id in list(data.keys()):
+    with open(json_dir + "customers.json", "r") as f:
+        json_data = json.load(f)
+
+    for entry in json_data:
+            if entry['chat_id'] == chat_id:
+                registered = True
+                break
+            else:
+                registered = False
+
+    if registered:
         bot.sendMessage(chat_id, "Du bist schon registriert")
     else:
-        bot.sendMessage(chat_id, "Vorname?", reply_markup=telegram.ForceReply(True))
+        bot.sendMessage(chat_id, "Geben Sie bitte Ihren Vornamen ein", reply_markup=telegram.ForceReply(True))
         data[chat_id] = 0
         if chat_id not in list(users.keys()):
             users[chat_id] = {
@@ -45,49 +57,49 @@ def reply(bot, update):
     # Vorname
     if data[chat_id] == 0:
         users[chat_id]["firstname"] = update.message.text
-        bot.sendMessage(chat_id, "Nachname?", reply_markup=telegram.ForceReply(True))
+        bot.sendMessage(chat_id, "Bitte geben Sie Ihren Nachnamen ein", reply_markup=telegram.ForceReply(True))
         data[chat_id] = 1
 
     # Nachname
     elif data[chat_id] == 1:
         users[chat_id]["lastname"] = update.message.text
-        bot.sendMessage(chat_id, "EMail?", reply_markup=telegram.ForceReply(True))
+        bot.sendMessage(chat_id, "Was ist Ihre E-Mail Adresse?", reply_markup=telegram.ForceReply(True))
         data[chat_id] = 2
 
     # Email
     elif data[chat_id] == 2:
         users[chat_id]["email"] = update.message.text
-        bot.sendMessage(chat_id, "Password?", reply_markup=telegram.ForceReply(True))
+        bot.sendMessage(chat_id, "Vergeben Sie bitte ein sicheres Passwort", reply_markup=telegram.ForceReply(True))
         data[chat_id] = 3
 
     # Password
     elif data[chat_id] == 3:
         users[chat_id]["password"] = update.message.text
-        bot.sendMessage(chat_id, "Stadt?", reply_markup=telegram.ForceReply(True))
+        bot.sendMessage(chat_id, "In welcher Stadt haben Sie Ihren Wohnsitz?", reply_markup=telegram.ForceReply(True))
         data[chat_id] = 4
 
     # Stadt
     elif data[chat_id] == 4:
         users[chat_id]["contact"]["city"] = update.message.text
-        bot.sendMessage(chat_id, "PLZ?", reply_markup=telegram.ForceReply(True))
+        bot.sendMessage(chat_id, "Wie lautet die Postleitzahl Ihres Wohnortes?", reply_markup=telegram.ForceReply(True))
         data[chat_id] = 5
 
     # PLZ
     elif data[chat_id] == 5:
         users[chat_id]["contact"]["postcode"] = update.message.text
-        bot.sendMessage(chat_id, "Straße?", reply_markup=telegram.ForceReply(True))
+        bot.sendMessage(chat_id, "In welcher Stra�e wohnen Sie? (ohne Hausnummer)", reply_markup=telegram.ForceReply(True))
         data[chat_id] = 6
 
-    # Straße
+    # Strasse
     elif data[chat_id] == 6:
         users[chat_id]["contact"]["street"] = update.message.text
-        bot.sendMessage(chat_id, "Nr?", reply_markup=telegram.ForceReply(True))
+        bot.sendMessage(chat_id, "Hausnummer", reply_markup=telegram.ForceReply(True))
         data[chat_id] = 7
 
     # Nr
     elif data[chat_id] == 7:
         users[chat_id]["contact"]["nr"] = update.message.text
-        bot.sendMessage(chat_id, "Phone?", reply_markup=telegram.ForceReply(True))
+        bot.sendMessage(chat_id, "Unter welcher Nummer sind Sie erreichbar?", reply_markup=telegram.ForceReply(True))
         data[chat_id] = 8
 
     # Phone
